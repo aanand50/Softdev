3392598505d71512f8ad31c1278bd76454fd5e43
Aditya Anand <aanand50@stuy.edu>
2024-09-26 20:35:22 -0400

    revisited wk11

--- 11_flask-static/app.py
+++ 11_flask-static/app.py
@@ -1,9 +1,11 @@
-# Clyde 'Thluffy' Sinclair
-# SoftDev
-# Sep 2024
+'''Caden Khuu, Aditya Anand, Danny Huang
+  CAD
+  SoftDev
+  Flask and HTML
+  2024-9-26
+  time spent: 0.5 hours
+'''
 
-# DEMO
-# basics of /static folder
 import random
 from flask import Flask
 app = Flask(__name__)
@@ -14,15 +16,20 @@ def hello_world():
     print(__name__)
     return "No hablo queso!"
 
-'''
 @app.route("/static/foo.html")
-def h():
+def hello():
     print("the __name__ of this module is... ")
     print(__name__)
     return str(random.random())
-'''
 
+'''
+@app.route("/static/fixie.html")
+def yeah():
+    print("the __name__ of this module is... ")
+    print(__name__)
+    return "<h1>Team Name: CAD</h1><br><h2>Roster: Caden, Aditya, Danny</h2>"
+'''
+    
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
     app.run()
-
