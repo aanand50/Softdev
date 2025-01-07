2aed5ab384f1c5c67d503fa77d8f7cbc0bcf263e
Aditya Anand <144459162+aanand50@users.noreply.github.com>
2025-01-07 07:42:34 -0500

    Update fibfac.js

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -25,11 +25,21 @@ return (n * fa(n-1))}
 
 
 //fib:
+var fib = function(n) {
+  if (n == 0) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  } else {
+    return fib(n - 1) + fib(n - 2);
+  }
+}
 
 //<your team's fib(n) implementation>
 
 //TEST CALLS
 // (writing here can facilitate EZer copy/pasting into dev console now and later...)
-
+(fib 1)
+(fib 5)
 //=================================================================
 
